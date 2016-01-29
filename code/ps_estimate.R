dc18db041b05b37795052411d4e2bb2d3189a3bf
jlong <jlong@stat.tamu.edu>
2016-01-29 15:55:56 -0600

    NN=5 works well

--- code/ps_estimate.R
+++ code/ps_estimate.R
@@ -12,14 +12,15 @@ load("ps_sim.RData")
 
 period_min <- 0.2
 period_max <- 1
-mc.cores <- 10
+mc.cores <- 7
+NN <- 5
 
 newton_pest <- function(ii){
     print(ii)
     lc <- tms[[ii]]
     omegas <- get_freqs(period_min,period_max,.1/diff(range(lc[,1])))
     omegas <- omegas / (2*pi)
-    rss <- SawRss(lc[,2],lc[,1],omegas)
+    rss <- SawRss(lc[,2],lc[,1],omegas,NN=NN)
     return(1/omegas[which.min(rss)])
 }
 p_est_new <- unlist(mclapply(1:length(tms),newton_pest,mc.cores=mc.cores))
