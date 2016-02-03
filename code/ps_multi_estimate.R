e88161b952ffbe34c55d871374be7b4b18296d01
James Long <jlong@stat.tamu.edu>
2016-02-03 08:59:19 -0600

    testing sine model and various NN values

--- code/ps_multi_estimate.R
+++ code/ps_multi_estimate.R
@@ -2,6 +2,7 @@ rm(list=ls())
 set.seed(1234)
 
 library(parallel)
+library(multiband)
 
 ##source("func_multi_sine.R")
 source("func_multi_saw.R")
@@ -13,6 +14,22 @@ load("ps_multi_sim.RData")
 period_min <- 0.2
 period_max <- 1
 mc.cores <- 7
+
+
+tms <- tms[1:10]
+
+NN <- 1
+newton_pest <- function(ii){
+    print(ii)
+    lc <- tms[[ii]]
+    omegas <- get_freqs(period_min,period_max,.1/diff(range(lc[,1])))
+    omegas <- omegas / (2*pi)
+    rss <- SawRss(m=lc$m,t=lc$t,ampj=lc$ampj,phij=lc$phij,omegas=omegas,NN=NN)
+    return(1/omegas[which.min(rss)])
+}
+p_est_new1 <- unlist(mclapply(1:length(tms),newton_pest,mc.cores=mc.cores))
+
+
 NN <- 5
 newton_pest <- function(ii){
     print(ii)
@@ -22,23 +39,29 @@ newton_pest <- function(ii){
     rss <- SawRss(m=lc$m,t=lc$t,ampj=lc$ampj,phij=lc$phij,omegas=omegas,NN=NN)
     return(1/omegas[which.min(rss)])
 }
-p_est_new <- unlist(mclapply(1:length(tms),newton_pest,mc.cores=mc.cores))
+p_est_new5 <- unlist(mclapply(1:length(tms),newton_pest,mc.cores=mc.cores))
 
 
-## K <- 1
-## sine_pest <- function(ii){
-##     print(ii)
-##     lc <- tms[[ii]]
-##     omegas <- get_freqs(period_min,period_max,.1/diff(range(lc[,1])))
-##     lc <- to_list_lc(lc)
-##     rss <- matrix(0,nrow=length(omegas),ncol=length(lc))
-##     for(jj in 1:length(lc)){
-##         rss[,jj] <- vapply(omegas,compute_rss,c(0),K,lc[[jj]])
-##     }
-##     rss <- rowSums(rss)
-##     return((2*pi)/omegas[which.min(rss)])
-## }
-## p_est_sine <- unlist(mclapply(1:length(tms),sine_pest,mc.cores=mc.cores))
+NN <- 10
+newton_pest <- function(ii){
+    print(ii)
+    lc <- tms[[ii]]
+    omegas <- get_freqs(period_min,period_max,.1/diff(range(lc[,1])))
+    omegas <- omegas / (2*pi)
+    rss <- SawRss(m=lc$m,t=lc$t,ampj=lc$ampj,phij=lc$phij,omegas=omegas,NN=NN)
+    return(1/omegas[which.min(rss)])
+}
+p_est_new10 <- unlist(mclapply(1:length(tms),newton_pest,mc.cores=mc.cores))
 
 
-save(tms,periods,p_est_new,file="ps_multi_estimate.RData")
+sine_pest <- function(ii){
+    print(ii)
+    omegas <- get_freqs(period_min,period_max,.1/diff(range(tms[[ii]][,1])))
+    lc <- split(tms[[ii]],tms[[ii]]$ampj)
+    out <- pgls(lc,periods = (2*pi)/omegas,BCD_flag=FALSE)
+    return(out$period_seq_all[which.min(out$rss_ls)])
+}
+p_est_sine <- unlist(mclapply(1:length(tms),sine_pest,mc.cores=mc.cores))
+
+
+save(tms,periods,p_est_new1,p_est_new5,p_est_new10,p_est_sine,file="ps_multi_estimate.RData")
