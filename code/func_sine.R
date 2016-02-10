42228a3b23ea46e34a065860fdb4ef0e061defef
James Long <jlong@stat.tamu.edu>
2016-02-10 14:25:34 -0600

    added function to func_sine

--- code/func_sine.R
+++ code/func_sine.R
@@ -14,3 +14,15 @@ compute_rss <- function(w,K,lc){
     beta <- compute_params(w,K,lc[,2],X)
     return(sum((lc[,2] - X%*%beta)^2))
 }
+
+
+get_sinusoidal_params <- function(beta){
+    beta0 <- beta[1]
+    amp <- rep(0,(length(beta)-1)/2)
+    rho <- rep(0,(length(beta)-1)/2)
+    for(ii in 1:length(amp)){
+        amp[ii] <- sqrt(beta[2*ii]^2 + beta[2*ii + 1]^2)
+        rho[ii] <- atan2(beta[2*ii],beta[2*ii+1])
+    }
+    return(list(beta0=beta0,amp=amp,rho=rho))
+}
