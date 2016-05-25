294f4b12db0e663d475a05b1981cebbc58579334
James Long <jlong@stat.tamu.edu>
2016-05-25 10:01:15 -0500

    vcing template creation

--- code/make_template/make_template.R
+++ code/make_template/make_template.R
@@ -1,68 +1,30 @@
 rm(list=ls())
-
-source('func.R')
-
-library(RColorBrewer)
-library(scales)
-
-rrlyrae <- read.table("apj326724t2_mrt.txt",skip=42)
-rrlyrae <- rrlyrae[rrlyrae[,2] == "ab",]
-
-folder <- "../rrlyrae"
-fnames <- list.files(folder)
-
-
-## order fnames and rrlyrae
-rrlyrae[,1] <- paste("LC_",rrlyrae[,1],".dat",sep="")
-fnames <- fnames[fnames %in% rrlyrae[,1]]
-rrlyrae <- rrlyrae[rrlyrae[,1] %in% fnames,]
-rrlyrae <- rrlyrae[order(rrlyrae[,1]),]
-fnames <- fnames[order(fnames)]
-
-
-## load light curves and put in tmss format
-lcs <- list()
-for(ii in 1:length(fnames)){
-    lcs[[ii]] <- read.table(paste(folder,fnames[ii],sep="/"))
-}
-for(ii in 1:length(lcs)) names(lcs[[ii]]) <- c("time","b","mag","sigma")
-
-
-LCtoTMS <- function(lc){
-    lc[,1] <- lc[,1] - min(lc[,1])
-    levs <- levels(lc$b)
-    tms <- list()
-    for(ii in 1:length(levs)){
-        tms[[ii]] <- lc[lc$b == levs[ii],c("time","mag","sigma")]
-        names(tms[[ii]]) <- c("time","mag","sigma")
-    }
-    names(tms) <- levs
-    return(tms)
-}
-tmss <- lapply(lcs,LCtoTMS)
-
+unlink("*.RData")
+unlink("*.pdf")
+source('../common/funcs.R')
+load("../data/clean/sdss_rrab.RData")
 
 
 ## get light curves with at least 50 observations / band in each band
-bands <- names(tmss[[1]])
-nobs <- matrix(0,nrow=length(tmss),ncol=5)
+bands <- names(tms[[1]])
+nobs <- matrix(0,nrow=length(tms),ncol=length(bands))
 for(ii in 1:length(bands)){
-    nobs[,ii] <- vapply(tmss,function(x){nrow(x[[bands[ii]]])},c(0))
+    nobs[,ii] <- vapply(tms,function(x){nrow(x[[bands[ii]]])},c(0))
 }
 to_use <- rowSums(nobs > 45) == 5
-tmss <- tmss[to_use]
-rrlyrae <- rrlyrae[to_use,]
+tms <- tms[to_use]
+periods <- periods[to_use]
 
 
 ### smooth lightcurves using supersmoother
 ### place on equally spaced grid
 N <- 100
 t <- (1:N)/N
-lc_grid <- array(0,c(length(tmss),N,5),dimnames=list(NULL,NULL,bands))
-for(ii in 1:length(tmss)){
+lc_grid <- array(0,c(length(tms),N,5),dimnames=list(NULL,NULL,bands))
+for(ii in 1:length(tms)){
     for(jj in 1:length(bands)){
-        lc <- tmss[[ii]][[bands[jj]]]
-        lc[,1] <- (lc[,1] %% rrlyrae[ii,3]) / rrlyrae[ii,3]
+        lc <- tms[[ii]][[bands[jj]]]
+        lc[,1] <- (lc[,1] %% periods[ii]) / periods[ii]
         temp <- supsmu(lc[,1],lc[,2],periodic=TRUE)
         ymean <- mean(c(temp$y[1],temp$y[length(temp$y)]))
         temp$y <- c(ymean,temp$y,ymean)
@@ -102,7 +64,7 @@ rs <- t(t(rs) - betas)
 
 
 ## construct lc with overall mean, dust, and band effects removed
-for(ii in 1:length(tmss)){
+for(ii in 1:length(tms)){
     for(jj in 1:5){
         lc_grid[ii,,jj] <- lc_grid[ii,,jj] - mean(lc_grid[ii,,jj]) + rs[ii,jj]
     }
@@ -112,7 +74,7 @@ for(ii in 1:length(tmss)){
 
 amps <- apply(lc_grid,c(1,3),function(x){mean(abs(x))})
 
-pairs(cbind(amps,period=rrlyrae[,3]))
+##pairs(cbind(amps,periods))
 
 sv <- svd(amps)
 pred <- sv$d[1]*sv$u[,1,drop=FALSE]%*%matrix(sv$v[,1],ncol=5)
@@ -122,7 +84,7 @@ names(amps) <- bands
 
 ## phase (initial) and amplitude registration
 ## improved phase registration using squared differences next
-for(ii in 1:length(tmss)){
+for(ii in 1:length(tms)){
     temp <- lc_grid[ii,,1]
     ix <- which.min(temp)
     if(ix > 1.5){
@@ -151,8 +113,8 @@ for(JJ in 1:5){
 
 ## visualize curves with templates
 ## cols <- brewer.pal(10,name="RdBu")
-## decLocations <- quantile(rrlyrae[,3], probs = seq(0.1,0.9,by=0.1),type=4)
-## dec <- findInterval(rrlyrae[,3],c(-Inf,decLocations, Inf))
+## decLocations <- quantile(periods, probs = seq(0.1,0.9,by=0.1),type=4)
+## dec <- findInterval(periods,c(-Inf,decLocations, Inf))
 ## yedge <- 2.5
 ## del <- phase(lc_grid[,,1])
 ## for(JJ in 1:5){
@@ -162,7 +124,7 @@ for(JJ in 1:5){
 ##     ylim <- range(lc_grid[,,JJ])
 ##     ylim <- c(-yedge,yedge)
 ##     plot(0,0,ylim=ylim,xlim=c(0,1),col=0,xlab="phase",ylab="mag",xaxs="i")
-##     for(ii in 1:length(tmss)){
+##     for(ii in 1:length(tms)){
 ##         points(t,lc_grid[ii,,JJ],type='l',col=cols[dec[ii]])
 ##     }
 ##     ylim <- range(x_new[[JJ]])
@@ -223,4 +185,16 @@ templatesd <- t(apply(templates,1,function(x){ComputeDerivative(x,len)}))
 
 tem <- list(betas=betas,dust=dust,
             templates=templates,templatesd=templatesd)
-save(tem,file="make_template.RData")
+
+## functions for interpolating templates
+temp_time <- seq(0,1,length.out=ncol(tem$templates))
+tem$template_funcs <- list()
+for(jj in 1:nrow(tem$templates)){
+    tem$template_funcs[[jj]] <- approxfun(temp_time,tem$templates[jj,])
+}
+tem$templated_funcs <- list()
+for(jj in 1:nrow(tem$templatesd)){
+    tem$templated_funcs[[jj]] <- approxfun(temp_time,tem$templatesd[jj,])
+}
+
+save(tem,file="template.RData")
