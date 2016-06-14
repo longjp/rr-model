c6bc660a48a069ea8e3a4d86529000a52ff45d25
James Long <jlong@stat.tamu.edu>
2016-06-14 16:56:37 -0500

    reverted to earlier make template

--- code/make_template/make_template.R
+++ code/make_template/make_template.R
@@ -3,7 +3,6 @@ unlink("*.RData")
 unlink("*.pdf")
 source('../common/funcs.R')
 load("../data/clean/sdss_rrab.RData")
-library(RColorBrewer)
 
 
 ## get light curves with at least 50 observations / band in each band
@@ -16,13 +15,6 @@ for(ii in 1:length(bands)){
 to_use <- rowSums(nobs > 45) == 5
 tms <- tms[to_use]
 periods <- periods[to_use]
-
-rrlyrae <- read.table("../analysis/sdss_sim_class/apj326724t3_mrt.txt",skip=30)
-names(rrlyrae)[1:5] <- c("ID","ra","dec","ar","d")
-rrlyrae$ID <- paste0("LC_",rrlyrae$ID,".dat")
-rrlyrae <- rrlyrae[rrlyrae$ID %in% names(tms),]
-rrlyrae <- rrlyrae[order(rrlyrae$ID),]
-
 
 
 ### smooth lightcurves using supersmoother
@@ -55,30 +47,7 @@ dust <- dat[,2]
 names(dust) <- dat[,1]
 dust <- dust[order(names(dust))]
 
-## set up design matrix X
-mstack <- as.vector(t(m))
-lcid <- as.factor(rep(1:nrow(m),each=ncol(m)))
-bandid <- as.factor(rep(1:ncol(m),nrow(m)))
-periodslog10 <- rep(log10(periods),each=ncol(m))
-periodslog102 <- rep(log10(periods)^2,each=ncol(m))
-dustid <- rep(dust,nrow(m))
-
-## bandid first column not being produced
-X <- model.matrix(~lcid+bandid+bandid:periodslog10+bandid:periodslog102+lcid:dustid-1)
-
-2*nrow(m) + 3*ncol(m)
-ncol(X)
-
-a <- as.factor(c("a","b","c","a","b","c"))
-b <- as.factor(c(1,1,1,2,2,2))
-model.matrix(~a+b-1,contrasts = list(a = "contr.sum",b="contr.sum"))
-model.matrix(~a+b-1)
-
-lm.fit <- lm(mstack~X-1)
-
-length(lm.fit$coefficients)
-
-## load and compute beta (M_x) parameters
+## load and compute beta (M_x) parameters using median of periods
 rrmag <- read.table("rrmag.dat",header=TRUE)
 rrmag <- rrmag[rrmag$Sys=="SDSS",]
 rrmag <- rrmag[order(rrmag$bnd),]
@@ -86,108 +55,12 @@ lpmed <- log10(median(periods))
 betas <- rrmag$c0 + rrmag$p1*(lpmed + 0.2) + rrmag$p2*(lpmed + 0.2)^2
 names(betas) <- rrmag$bnd
 
-
-#### compare distances with sesar 2010 using exact periods and median of periods
-lpmed <- log10(median(periods))
-betas <- rrmag$c0 + rrmag$p1*(lpmed + 0.2) + rrmag$p2*(lpmed + 0.2)^2
-names(betas) <- rrmag$bnd
-d <- rep(0,nrow(m))
-m_shift <- t(t(m) - betas)
-for(ii in 1:nrow(m)){
-    d[ii] <- lm(m_shift[ii,]~dust)$coefficients[1]
-}
-pdf("distance_median.pdf")
-plot(rrlyrae$d,10^(d/5 + 1)/1000,
-     xlab="Sesar 2010 Distance",
-     ylab="Estimated Distance",
-     main="Distance Estimate using Median Period for Absolute Magnitude")
-abline(a=0,b=1)
-dev.off()
-
-
-d <- rep(0,nrow(m))
-lpmed <- log10(periods)
-for(ii in 1:nrow(m)){
-    betas <- rrmag$c0 + rrmag$p1*(lpmed[ii] + 0.2) + rrmag$p2*(lpmed[ii] + 0.2)^2
-    m_shift <- m[ii,] - betas
-    d[ii] <- lm(m_shift~dust)$coefficients[1]
-}
-pdf("distance_exact.pdf")
-plot(rrlyrae$d,10^(d/5 + 1)/1000,
-     xlab="Sesar 2010 Distance",
-     ylab="Estimated Distance",
-     main="Distance Estimate using Exact Period")
-abline(a=0,b=1)
-dev.off()
-
-
-
-eb_v <- rep(0,nrow(m))
-lpmed <- log10(periods)
-for(ii in 1:nrow(m)){
-    betas <- rrmag$c0 + rrmag$p1*(lpmed[ii] + 0.2) + rrmag$p2*(lpmed[ii] + 0.2)^2
-    m_shift <- m[ii,] - betas
-    eb_v[ii] <- lm(m_shift~dust)$coefficients[2]
-}
-hist(eb_v)
-
-
-
-
-cols <- brewer.pal(10,name="RdBu")
-decLocations <- quantile(periods, probs = seq(0.1,0.9,by=0.1),type=4)
-dec <- findInterval(periods,c(-Inf,decLocations, Inf))
-
-
 ## estimate d and alpha for each lc
 rs <- matrix(0,nrow=nrow(m),ncol=ncol(m))
-mMean <- colMeans(m)
-m_shift <- t(t(m) - mMean)
+m_shift <- t(t(m) - betas)
 for(ii in 1:nrow(m)){
     rs[ii,] <- lm(m_shift[ii,]~dust)$residuals
 }
-colnames(rs) <- c(bands)
-pdf("residuals_independent.pdf")
-pairs(rs,main="Residual: Absolute Mag Independent of Period",col=cols[dec])
-dev.off()
-
-
-
-rrmag$p1[4] <- rrmag$p1[4] + .3
-rrmag$p1[1] <- rrmag$p1[1] + .3
-lpmed <- log10(periods)
-rs <- matrix(0,nrow=nrow(m),ncol=ncol(m))
-for(ii in 1:nrow(m)){
-    betas <- rrmag$c0 + rrmag$p1*(lpmed[ii] + 0.2) + rrmag$p2*(lpmed[ii] + 0.2)^2
-    m_shift <- m[ii,] - betas
-    rs[ii,] <- lm(m_shift~dust)$residuals
-}
-
-lim <- c(-.08,.08)
-colnames(rs) <- bands
-##pdf("residuals_dependent.pdf")
-dev.new()
-pairs(rs[,c("u","g","r","i","z")],main="Residual: Absolute Mag Dependent on Period",col=cols[dec],xlim=lim,ylim=lim)
-##dev.off()
-
-dev.new()
-plot(1:10,1:10,col=cols)
-dev.new()
-hist(periods)
-range(log10(periods))
-
-
-a <- matrix(1:4,ncol=2)
-a
-as.vector(t(a))
-mvec <- as.vector(t(m))
-pvec <- rep(log10(periods),each=length(bands))
-pvec2 <- pvec^2
-mu <- as.factor(rep(1:nrow(m),each=length(bands)))
-c0 <- as.factor(rep(1:length(bands),nrow(m)))
-
-
-lm.fit <- lm(mvec~mu+c0+pvec+pvec2
 
 #### NOTE: CORRELATION IN RESIDUALS FOR G AND U APPEARS RELATED TO PERIOD
 
