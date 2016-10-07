d035376a7d28a9efe1d96918b6e523e125daedbc
James Long <jlong@stat.tamu.edu>
2016-10-07 11:53:01 -0400

    writing pipeline to fit rr model and constant model to set of well sampled lcs

---
+++ code/data/sdss_class.R
@@ -0,0 +1,38 @@
+## downsample sdss stripe 82 rr lyrae ab to 50 total observations
+rm(list=ls())
+source('../common/funcs.R')
+
+rrlyrae <- read.table("raw/apj326724t2_mrt.txt",skip=42)
+rrlyrae <- rrlyrae[rrlyrae[,2] == "ab",]
+
+folder <- "raw/AllLCs/"
+fnames <- list.files(folder)
+fnames <- fnames[fnames!="LC_reorganize.tcl.dat"] ## get rid of non lc file
+
+## only use well sampled light curves
+nr <- vapply(fnames,function(x){nrow(read.table(paste(folder,x,sep="/")))},c(0))
+fnames <- fnames[nr > 75]
+
+## order fnames and rrlyrae
+Nnot <- 1000 ## number of non--rrlyrae to select
+rrlyrae[,1] <- paste("LC_",rrlyrae[,1],".dat",sep="")
+rrlyrae <- rrlyrae[order(rrlyrae[,1]),]
+fnot <- sample(fnames[!(fnames %in% rrlyrae[,1])],Nnot)
+fnames <- fnames[fnames %in% rrlyrae[,1]]
+fnames <- fnames[order(fnames)]
+rrlyrae <- rrlyrae[rrlyrae[,1] %in% fnames,]
+fnames <- c(fnames,fnot)
+periods <- c(rrlyrae[,3],rep(0,Nnot))
+cl <- c(rep("rr",nrow(rrlyrae)),rep("not",Nnot))
+
+## load light curves
+lcs <- vector("list",length(fnames))
+for(ii in 1:length(fnames)){
+    lcs[[ii]] <- read.table(paste(folder,fnames[ii],sep="/"))
+}
+for(ii in 1:length(lcs)) names(lcs[[ii]]) <- c("time","band","mag","sigma")
+tms <- lapply(lcs,LCtoTM)
+names(tms) <- fnames
+
+## output lightcurves and periods
+save(tms,periods,cl,file="clean/sdss_class.RData")
