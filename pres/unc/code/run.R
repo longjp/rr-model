753e2cb83c4e9a46fed3163c69735c0422f00ff7
James Long <jlong@stat.tamu.edu>
2016-10-26 10:31:34 -0400

    updated unc figs

--- pres/unc/code/run.R
+++ pres/unc/code/run.R
@@ -1,6 +1,11 @@
 ### run all code to make presentation plots
 
 
+print("running: plot-lcs.R")
 source("plot-lcs.R")
+
+print("running: density-bw-sim.R")
 source("density-bw-sim.R")
+
+print("running: classification-sim.R")
 source("classification-sim.R")
