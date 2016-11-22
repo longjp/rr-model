d81b812194deb4408364cb7c28da6d42b0fd9976
James Long <jlong@stat.tamu.edu>
2016-11-21 20:47:07 -0500

    rewrote python wrapper

--- code/fit_template/template.py
+++ code/fit_template/template.py
@@ -1,4 +1,5 @@
 ## python wrapper for template.R
+## by james long
 import numpy as np
 import rpy2.robjects as robjects
 from rpy2.robjects.vectors import IntVector, FloatVector, StrVector
@@ -6,44 +7,64 @@ import csv
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
+## set up rpy2 so we can call R functions
 r=robjects.r
 r.source("template.R")
-r.load("template.RData")
+r.load("template.RData") ## loads r.tem, see later code for usage
 FitTemplate=robjects.r['FitTemplate']
+ComputeCoeffs=robjects.r['ComputeCoeffs']
 
-## load a light curve
+## load a light curve, put in nice format
 fname="LC_4099.dat"
 with open(fname) as csvf:
     f = csv.reader(csvf,delimiter=' ')
     time, band, mag, error = zip(*f)
 
 time = np.array(time,dtype='float64')
+time = time - np.min(time) ## => time=0 is first observation
 mag = np.array(mag,dtype='float64')
 error = np.array(error,dtype='float64')
 
 ## create R dataframe using time,band,mag,error
 lc = robjects.r['TBMEtoLC'](FloatVector(time),StrVector(band),FloatVector(mag),FloatVector(error))
 
+
+###### fit model to lc
 ## choose frequency grid
 omegas=FloatVector(np.arange(start=1.0,stop=5.0,step=0.1/4000.0))
-
-## compute rss (residual sum of squares) for each frequency
+## compute rss (residual sum of squares) for each frequency, takes a minute
 rss=FitTemplate(lc,omegas,r.tem)
-## select best fitting period
-pest=1.0/omegas[np.argmin(rss)]
+## select best fitting period, ie lowest rss
+omega = omegas[np.argmin(rss)] ## best fit frequency
+pest=1.0/omega ## best fit period
+coeffs=ComputeCoeffs(lc,omega,r.tem) ## parameter estimates of best fit frequency
+## the output is [distance modulus (mu),amount of dust (E[B-V]),amplitude (a),phase (rho)]
+# >>> coeffs
+# R object with classes: ('numeric',) mapped to:
+# <FloatVector - Python:0x7fe29e3d4a08 / R:0x416c888>
+# [16.097936, 0.102392, 0.274502, 0.232388]
 
+###### now plot results
 ## plot lightcurve folded on estimated period
 cols = {'g': 'green', 'i': 'red', 'r':'blue','u': 'orange', 'z': 'yellow'}
-pts=plt.scatter(np.mod(time,pest),mag,color=map(cols.get,band))
+pts=plt.scatter(np.mod(time,pest),mag,color=list(map(cols.get,band)))
 plt.gca().invert_yaxis()
 plt.xlim([0,pest])
-class_colours = map(cols.get,band)
+class_colours = list(map(cols.get,band))
 recs = []
 for i in range(0,len(class_colours)):
     recs.append(mpatches.Rectangle((0,0),1,1,fc=class_colours[i]))
 
 plt.legend(recs,cols.keys(),loc=4)
+
+### add template fits
+gamma = r.tem[2]
+gamma = np.resize(a,(5,100))
+t=(np.arange(100)/100.)*pest
+ords=np.argsort((t - coeffs[3]*pest) % pest)
+for ii in np.arange(gamma.shape[0]):
+    m=coeffs[0] + r.tem[0][ii] + r.tem[1][ii]*coeffs[1] + coeffs[2]*gamma[ii,:]
+    plt.plot(t,m[ords],'k',color=cols[r.tem[0].names[ii]])
+
+
 plt.show()
-
-
-
