e7ec58cf350554f4f2aa3b0baeaf217e74924dcf
James Long <jlong@stat.tamu.edu>
2017-05-24 14:47:24 -0500

    wrote script to check how well model is fitting

---
+++ code/make_template/quality_check.R
@@ -0,0 +1,20 @@
+### fits well sampled light curves to templates, calculates median residuals
+### used to check that templates are working well, compare minor
+### implementation differences
+rm(list=ls())
+source('../common/funcs.R')
+source('../fit_template/fit_template.R')
+load("../data/clean/sdss_rrab.RData")
+load("../fit_template/template.RData")
+
+med_res <- vector("numeric",length(tms))
+for(ii in 1:length(med_res)){
+    lc <- TMtoLC(tms[[ii]])
+    coeffs <- ComputeCoeffs(lc,1/periods[ii],tem)
+    preds <- PredictTimeBand(lc[,1],lc[,2],1/periods[ii],coeffs,tem)
+    med_res[ii] <- median(abs(preds - lc[,3]))
+}
+
+
+hist(med_res)
+median(med_res) ## .031 seems reasonable for this number
