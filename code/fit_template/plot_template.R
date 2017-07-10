54e82b6814a54763bcadcc14edcbaa5da4018c8d
James Long <jlong@stat.tamu.edu>
2017-07-10 12:56:08 -0500

    matching colors with filter colors for plotting lightcurves

--- code/fit_template/plot_template.R
+++ code/fit_template/plot_template.R
@@ -7,9 +7,11 @@ t <- tem$temp_time
 ntemp <- nrow(templates)
 
 
-band_mark <- 1:6
-names(band_mark) <- c("u","g","r","i","z","Y")
-
+bandmark <- 1:6
+names(bandmark) <- c("u","g","r","i","z","Y")
+bandcol <- c("dodgerblue3","green","red",
+             "mediumorchid1","black","peachpuff4")
+names(bandcol) <- c("u","g","r","i","z","Y")
 
 ##plot of templates
 ylim <- rev(range(templates))
@@ -20,12 +22,12 @@ plot(0,0,col=0,ylim=ylim,xlim=xlim,xaxs='i',xlab="Phase",ylab="Normalized Magnit
      cex.lab=2)
 for(ii in 1:nrow(templates)){
     points(t,templates[ii,],type='l',lwd=3,
-           col=band_mark[rownames(templates)[ii]],
-           lty=band_mark[rownames(templates)[ii]])
+           col=bandcol[rownames(templates)[ii]],
+           lty=bandmark[rownames(templates)[ii]])
 }
-to_use <- names(band_mark) %in% rownames(templates)
-legend("bottomleft",names(band_mark)[to_use],
-       col=band_mark[to_use],lty=band_mark[to_use],lwd=3,cex=2)
+to_use <- names(bandmark) %in% rownames(templates)
+legend("bottomleft",names(bandmark)[to_use],
+       col=bandcol[to_use],lty=bandmark[to_use],lwd=3,cex=2)
 dev.off()
 
 
