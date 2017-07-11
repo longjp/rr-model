4fe390a15572fe48df70a97578ea1deebf792584
James Long <jlong@stat.tamu.edu>
2017-07-11 22:35:05 +0100

    running model on 60+ cores

--- code/analysis/params.R
+++ code/analysis/params.R
@@ -2,5 +2,5 @@
 N <- length(tms) ## number of lcs to analyze
 NN <- 10 ## number of newton steps
 omegas <- GetFreqs(0.2,1) ## frequency grid
-mc.cores <- 31 ## number of processors
+mc.cores <- 62 ## number of processors
 topN <- 5 ## number of periods to keep
