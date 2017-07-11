8fbad8446a309d77468256aec65de043c87a8600
James Long <jlong@stat.tamu.edu>
2017-07-11 22:34:25 +0100

    running pipeline on all lightcurves again

--- code/analysis/sdss_sim_class/fit.R
+++ code/analysis/sdss_sim_class/fit.R
@@ -16,10 +16,10 @@ source("../params.R")
 
 ## estimate periods for both the full lc (tms_FULL) and downsampled (tms)
 ## using both the RRL template and (period_est) and lomb-scarge (period_est_lomb)
-## period_est <- mclapply(1:N,FitTemplateParallel,
-##                        tms=tms,omegas=omegas,tem=tem,NN=NN,topN=topN,
-##                        mc.cores=mc.cores)
-## period_est <- matrix(unlist(period_est),ncol=topN,byrow=TRUE)
+period_est <- mclapply(1:N,FitTemplateParallel,
+                       tms=tms,omegas=omegas,tem=tem,NN=NN,topN=topN,
+                       mc.cores=mc.cores)
+period_est <- matrix(unlist(period_est),ncol=topN,byrow=TRUE)
 
 period_est_FULL <- mclapply(1:N,FitTemplateParallel,
                        tms=tms_FULL,omegas=omegas,tem=tem,NN=NN,topN=topN,
@@ -27,17 +27,15 @@ period_est_FULL <- mclapply(1:N,FitTemplateParallel,
 period_est_FULL <- matrix(unlist(period_est_FULL),ncol=topN,byrow=TRUE)
 
 
-save(period_est_FULL,file="results.RData")
+period_est_lomb <- mclapply(1:N,FitLombParallel,
+                            tms=tms,omegas=omegas,topN=topN,
+                            mc.cores=mc.cores)
+period_est_lomb <- matrix(unlist(period_est_lomb),ncol=topN,byrow=TRUE)
 
-## period_est_lomb <- mclapply(1:N,FitLombParallel,
-##                             tms=tms,omegas=omegas,topN=topN,
-##                             mc.cores=mc.cores)
-## period_est_lomb <- matrix(unlist(period_est_lomb),ncol=topN,byrow=TRUE)
-
-## period_est_lomb_FULL <- mclapply(1:N,FitLombParallel,
-##                             tms=tms_FULL,omegas=omegas,topN=topN,
-##                             mc.cores=mc.cores)
-## period_est_lomb_FULL <- matrix(unlist(period_est_lomb_FULL),ncol=topN,byrow=TRUE)
+period_est_lomb_FULL <- mclapply(1:N,FitLombParallel,
+                            tms=tms_FULL,omegas=omegas,topN=topN,
+                            mc.cores=mc.cores)
+period_est_lomb_FULL <- matrix(unlist(period_est_lomb_FULL),ncol=topN,byrow=TRUE)
 
 
-##save(period_est,period_est_lomb,period_est_FULL,period_est_lomb_FULL,file="results.RData")
+save(period_est,period_est_lomb,period_est_FULL,period_est_lomb_FULL,file="results.RData")
