258c8f586472fd382a8e736a9ced6b19ceaacead
James Long <jlong@stat.tamu.edu>
2017-07-18 03:00:25 +0100

    working on template for des filters

--- code/make_template/make_template_des.R
+++ code/make_template/make_template_des.R
@@ -3,10 +3,7 @@ load("../data/clean/sdss_rrab.RData")
 load("../fit_template/template.RData")
 source("../fit_template/fit_template.R")
 source("../common/funcs.R")
-
-##### make plot with sloan data in grey or very light, des darker
-##### print out every one of these light curves, sanity check
-##### estimate Y offset and shape, compare to z
+library(zoo)
 
 ## makes nice plot
 plotLC <- function(lc,p_est,tem,coeffs=NULL,add=FALSE,pch=TRUE){
@@ -104,7 +101,6 @@ names(coeffs) <- c("mu","ebv","amp","phase")
 ## plot folded light curve
 plotLC(lc_sdss,p_est,tem,coeffs)
 
-plotLC(lc_des,p_est,tem,add=TRUE,pch=FALSE)
 
 
 
@@ -112,14 +108,52 @@ plotLC(lc_des,p_est,tem,add=TRUE,pch=FALSE)
 coeffs <- matrix(0,nrow=length(tms),ncol=4)
 colnames(coeffs) <- c("mu","ebv","amp","phase")
 for(ii in 1:length(tms)){
-    lc <- TMtoLC(tms[[ii]])
+    lc <- lcs_sdss[[ii]]
     p_est <- periods[ii]
     omega_est <- 1/p_est
     coeffs[ii,] <- ComputeCoeffs(lc,omega_est,tem)
 }
 
-hist(coeffs[,2])
-summary(coeffs[,2])
+
+
+#####
+### add Y band template that is identical to z
+### this will be updated in make_template_des.R
+# absolute mags
+createY <- TRUE
+if(createY){
+    tem$betas['Y'] <- tem$betas['z']
+    tem$betas <- tem$betas[order(names(tem$betas))]
+    ## dust
+    tem$dust['Y'] <- tem$dust['z']
+    tem$dust <- tem$dust[order(names(tem$dust))]
+    ## model error
+    tem$model_error['Y'] <- tem$model_error['z']
+    tem$model_error <- tem$model_error[order(names(tem$model_error))]
+    ## templates
+    te <- matrix(0,nrow=nrow(tem$templates)+1,ncol=ncol(tem$templates))
+    rownames(te) <- c(rownames(tem$templates),"Y")
+    te[1:nrow(tem$templates),] <- tem$templates
+    te["Y",] <- te["z",]
+    te <- te[order(rownames(te)),]
+    tem$templates <- te
+    ## templatesd
+    te <- matrix(0,nrow=nrow(tem$templatesd)+1,ncol=ncol(tem$templatesd))
+    rownames(te) <- c(rownames(tem$templatesd),"Y")
+    te[1:nrow(tem$templatesd),] <- tem$templatesd
+    te["Y",] <- te["z",]
+    te <- te[order(rownames(te)),]
+    tem$templatesd <- te
+    ## tempalate_funcs
+    tem$template_funcs["Y"] <- tem$template_funcs["z"]
+    tem$template_funcs <- tem$template_funcs[order(names(tem$template_funcs))]
+    ## tempalated_funcs
+    tem$templated_funcs["Y"] <- tem$templated_funcs["z"]
+    tem$templated_funcs <- tem$templated_funcs[order(names(tem$templated_funcs))]
+}
+
+
+
 
 
 ## set dust to new values
@@ -127,31 +161,60 @@ extc <- read.table("extc.dat",stringsAsFactors=FALSE)
 extc <- extc[extc[,1]=="DES",c(2,3)]
 tem$dust[extc$V2] <- extc$V3
 
+## set extinction to new values
 
 
-### TODO: at minimum DES templates should use DES dust from extc
-###       changes to shape of templates may make sense as well
+unlink("quality_check_des",recursive=TRUE)
+dir.create("quality_check_des")
 
 
-
-### how much of a problem is aliasing
-times <- vector("list",length(lcs_des))
-for(ii in 1:length(times)){
-    temp <- sort(lcs_des[[ii]][,1])
-    n <- length(temp)
-    times[[ii]] <- temp[2:n] - temp[1:(n-1)]
+lcs_resid <- lcs_des
+for(ii in 1:length(lcs_des)){
+    omega_est <- 1/periods[ii]
+    lcs_resid[[ii]][,1] <- (lcs_des[[ii]][,1]*omega_est + coeffs[ii,4]) %% 1.0
+    lcs_resid[[ii]][,3] <- lcs_des[[ii]][,3] - PredictTimeBand(lcs_des[[ii]][,1],lcs_des[[ii]][,2],omega_est,coeffs[ii,],tem)
 }
-times <- unlist(times)
-hist(times %% 1,main="des",breaks=seq(0,1,.025))
 
 
-times <- vector("list",length(lcs_des))
-for(ii in 1:length(times)){
-    temp <- sort(lcs_sdss[[ii]][,1])
-    n <- length(temp)
-    times[[ii]] <- temp[2:n] - temp[1:(n-1)]
+
+lcs_resid <- do.call(rbind,lcs_resid)
+
+bs <- unique(lcs_resid$band)
+
+for(ii in 1:length(bs)){
+    lcs_resid_band <- lcs_resid[lcs_resid$band==bs[ii],]
+    pdf(paste0("quality_check_des/residuals_",bs[ii],"_new.pdf"))
+    plot(0,0,ylim=c(.3,-.3),
+         xlab="phase",ylab="magnitude residual",
+         xlim=c(0,2),xaxs='i',col=0,main=paste0(bs[ii]," band residuals"))
+    lc1 <- lcs_resid_band
+    lc1 <- lc1[order(lc1[,1]),]
+    lc2 <- lc1
+    lc2[,1] <- lc1[,1] + 1
+    lc_temp <-rbind(lc1,lc2)
+    points(lc_temp$time,lc_temp$mag,
+           col="#00000030")
+
+    out <- rollmedian(lc_temp[,3],51,na.pad=TRUE)
+    abline(h=0,lwd=2)
+    points(lc_temp[,1],out,type='l',col='red',lwd=2)
+    dev.off()
 }
-times <- unlist(times)
-dev.new()
-hist(times %% 1,main="sdss",breaks=seq(0,1,.025))
-### conclusion: aliasing will be problem with des, but not as much as with sdss
+
+
+
+
+
+
+
+
+
+
+
+
+hist(coeffs[,2])
+summary(coeffs[,2])
+
+
+
+
