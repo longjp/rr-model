7812b520cff91c2b0db8ebc1c239f8fd032e294e
James Long <jlong@stat.tamu.edu>
2017-07-19 11:32:33 +0100

    added main argument to plotLC

--- code/common/plot_funcs.R
+++ code/common/plot_funcs.R
@@ -1,4 +1,4 @@
-plotLC <- function(lc,p_est,coeffs,tem){
+plotLC <- function(lc,p_est,coeffs,tem,main=NULL){
     bandpch <- 1:6
     names(bandpch) <- c("u","g","r","i","z","Y")
     bandcol <- c("dodgerblue3","green","red",
@@ -9,12 +9,18 @@ plotLC <- function(lc,p_est,coeffs,tem){
     lc2 <- lc1
     lc2[,1] <- lc1[,1] + 1
     lc_temp <-rbind(lc1,lc2)
-    par(mar=c(5,5,1,1))
+    if(is.null(main)){
+        par(mar=c(5,5,1,1))
+        main <- ""
+    }
+    else {
+        par(mar=c(5,5,5,1))
+    }
     plot(lc_temp$time,lc_temp$mag,
          col=bandcol[lc_temp$band],pch=bandpch[lc_temp$band],
          ylim=rev(range(lc_temp$mag)),
          xlab="Phase",ylab="Magnitude",
-         xlim=c(0,2),xaxs='i',cex.axis=1.5,cex.lab=1.5)
+         xlim=c(0,2),xaxs='i',cex.axis=1.5,cex.lab=1.5,main=main)
     segments(lc_temp$time,
              lc_temp$mag+lc_temp$error,
              lc_temp$time,
