4089396716c6b157bb8ffbcff0c7da8680fa486a
James Long <jlong@stat.tamu.edu>
2017-07-20 12:17:58 -0400

    improved python code for kappa feature

--- code/fit_template/feat.py
+++ code/fit_template/feat.py
@@ -3,14 +3,17 @@
 import numpy as np
 from numpy import random
 
-def kappa(phi):
-    n = np.size(phi)
-    sumcos = np.sum(np.cos(2*np.pi*phi))
-    sumsin = np.sum(np.sin(2*np.pi*phi))
-    rbar = np.sqrt(sumcos*sumcos + sumsin*sumsin) / n
-    rbar2 = np.power(rbar,2)
-    return (rbar*(2.-rbar2)) / (1.-rbar2)
 
+## arguments
+##        phi : vector of phases on [0,1)
+## value
+##        1-step mle of kappa from
+##        fisher von mises dist on unit circle
+def kappa(phi):
+    x = np.sum(np.cos(2*np.pi*phi))
+    y = np.sum(np.sin(2*np.pi*phi))
+    rbar = np.sqrt(x*x + y*y) / np.size(phi)
+    return (rbar*(2.-rbar**2)) / (1.-rbar**2)
 
 ## generate some random data on [0,1)
 mu = 0.5
@@ -34,4 +37,3 @@ sigma = .02
 phi = np.random.normal(mu, sigma, 1000)
 phi = phi % 1.0
 kappa(phi)
-     
