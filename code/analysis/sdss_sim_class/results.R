9a24ca8d8cc23cc7d4aeef8d370cb1ed08ad4ff0
James Long <jlong@stat.tamu.edu>
2017-09-21 15:20:47 -0500

    improved plotting for results

--- code/analysis/sdss_sim_class/results.R
+++ code/analysis/sdss_sim_class/results.R
@@ -7,7 +7,7 @@ load("../../fit_template/template_sdss.RData")
 source("../../fit_template/fit_template.R")
 source("../../common/funcs.R")
 source("../funcs.R")
-
+source("../../common/plot_funcs.R")
 
 ## data source
 load("../../data/clean/sdss_sim_class.RData")
@@ -80,52 +80,14 @@ abline(a=0,b=1)
 dev.off()
 
 
-## plot all bands with best fit parameters, store in figs
-for(ii in 1:length(tms)){
-    tm <- tms[[ii]]
-    omega <- 1/period_est[ii]
-    coeffs <- ComputeCoeffs(TMtoLC(tm),omega,tem)
-    pdf(paste0("figs/",ii,".pdf"),height=12,width=8)
-    par(mar=c(3,4,2,1),mfcol=c(5,1))
-    for(jj in names(tem$dust)){
-        pred <- (coeffs[1] + tem$betas[jj] + coeffs[2]*tem$dust[jj]
-            + coeffs[3]*tem$template_funcs[[jj]]((tem$temp_time + coeffs[4]) %% 1))
-        xlim <- range(tem$temp_time/omega)
-        ylim <- range(range(pred),tm[[jj]]$mag)
-        plot(tem$temp_time/omega,pred,type='l',xlab="Phase",ylab="Mag",ylim=rev(ylim),
-         xlim=xlim,main=paste0(jj," band"))
-        if(!is.null(tm[[jj]])){
-            points((tm[[jj]]$time %% (1/omega)),tm[[jj]]$mag)
-        }
-    }
-    dev.off()
-}
-
 ## make all plots together
 for(ii in 1:length(tms)){
-    bands <- names(tem$dust)
-    bands <- sort(bands)
-    omega <- 1/period_est[ii]
     tm <- tms[[ii]]
     lc <- TMtoLC(tm)
+    p_est <- period_est[ii]
+    omega <- 1 / p_est
     coeffs <- ComputeCoeffs(lc,omega,tem)
-    preds <- list()
-    for(jj in 1:length(bands)){
-        preds[[jj]] <- (coeffs[1] + tem$betas[jj] + coeffs[2]*tem$dust[jj]
-            + coeffs[3]*tem$template_funcs[[jj]]((tem$temp_time + coeffs[4]) %% 1))
-    }
-    ylim <- range(range(preds),range(lc$mag))
-    xlim <- range((lc$time %% (1/omega))*omega)
     pdf(paste0("figs/",ii,"_one.pdf"),height=8,width=12)
-    par(mar=c(5,5,1,1))
-    plot(0,0,col=0,ylim=rev(ylim),xlim=xlim,xlab="Phase",ylab="Magnitude",cex.lab=1.5)
-    for(jj in 1:length(bands)){
-        temp <- lc[lc$band==bands[jj],]
-        if(nrow(temp) > 0.5){
-            points((temp$time %% (1/omega)) * omega,temp$mag,col=jj,pch=jj,cex=1.5)
-        }
-        points(tem$temp_time,preds[[jj]],type='l',col=jj,lwd=3,lty=jj)
-    }
-    legend("bottomleft",bands,col=1:length(bands),lty=1:length(bands),lwd=3,cex=1.5)
+    plotLC(lc,p_est,coeffs,tem,main=NULL,tem_only=TRUE)
     dev.off()
 }
