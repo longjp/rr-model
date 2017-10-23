3ffd2c4b8f07b1b0db80fe0567bf86f24d607ac5
James Long <jlong@stat.tamu.edu>
2017-10-23 17:10:24 -0500

    getting schlegel extinction values

--- code/data/get_extinct.R
+++ code/data/get_extinct.R
@@ -16,15 +16,3 @@ while(nn*ii < nrow(cat)){
     write.table(radec[b:e,],file=fname,row.names=FALSE,col.names=FALSE,sep="\t",append=TRUE)
     ii <- ii + 1
 }
-
-
-
-temp <- read.table("raw/apj326724t3_mrt.txt",skip=30) ## get distances for rrlyrae, in different file
-
-extc <- read.table("../make_template/extc.dat")
-head(extc)
-Rrsdss <- extc[extc[,1]=="SDSS" & extc[,2]=="r",][,3]
-
-temp[1,4]/2.751
-
-ebv*R_r = extinction in r
