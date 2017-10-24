8938e2f3229a6ae2a729fac915d3e655340cde1d
James Long <jlong@stat.tamu.edu>
2017-10-23 23:15:49 -0500

    vcing extinction values

--- code/data/extc/table_convert.py
+++ code/data/extc/table_convert.py
@@ -1,7 +1,7 @@
 ## converts tables in ipac form to csv
 from astropy.table import Table
-for i in range(2):
-    fname_in = 'radec_values/' + str(i) + '.tbl'
-    fname_out = 'radec_values/' + str(i) + '.txt'
+for i in range(7):
+    fname_in = 'radec_extc/' + str(i) + '.tbl'
+    fname_out = 'radec_extc/' + str(i) + '.txt'
     t = Table.read(fname_in, format='ipac')
     t.write(fname_out, format='csv')
