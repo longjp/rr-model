6af5fbff75801352624513ec9738bd72e95a1e65
James Long <jlong@stat.tamu.edu>
2017-10-23 23:19:17 -0500

    fixed read.table file, saving files as .dat

--- code/data/extc/get_extinct.R
+++ code/data/extc/get_extinct.R
@@ -1,13 +1,13 @@
 rm(list=ls())
 unlink("radec",recursive=TRUE)
 dir.create("radec")
-cat <- read.table("raw/stripe82candidateVar_v1.1.dat",header=TRUE)
+cat <- read.table("../raw/stripe82candidateVar_v1.1.dat",header=TRUE)
 radec <- cat[,c(2,3)]
 
 nn <- 10000
 ii <- 0
 while(nn*ii < nrow(cat)){
-    fname <- paste0("radec/radec",ii,".txt")
+    fname <- paste0("radec/radec",ii,".dat")
     unlink(fname)
     cat("| ra\t| dec\t|\n",file = fname,append = TRUE)
     cat("| double\t| double\t|\n",file = fname,append = TRUE)
