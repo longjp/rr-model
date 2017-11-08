6b72624bc828f8ffaaab1c3d34f027964f2af01c
James Long <jlong@stat.tamu.edu>
2017-11-08 16:34:02 -0600

    comparing distance estimates when period is correct / wrong

--- code/analysis/sdss_sim_class/2-class-dist.R
+++ code/analysis/sdss_sim_class/2-class-dist.R
@@ -119,6 +119,9 @@ pairs(cbind("amp"=log10(features[,"a"]),
             "dev"=log10(features[,"rss.n"])),
       col=cl.plot,pch=cl.plot)
 
+
+
+
 ##pdf(paste0(fig.dir,"/a_vs_dev_log.pdf"),height=6,width=7)
 par(mar=c(5,5,1,1))
 plot(features[,3],features[,6],col=cl.plot,pch=cl.plot,
@@ -126,6 +129,7 @@ plot(features[,3],features[,6],col=cl.plot,pch=cl.plot,
      log="xy",xlim=c(.01,5))
 legend("topleft",c("RR Lyrae","Not RR Lyrae"),col=2:1,pch=2:1,cex=1.5)
 dev.new()
+
 par(mar=c(5,5,1,1))
 plot(features[,3],features[,7],col=cl.plot,pch=cl.plot,
      xlab="Amplitude",ylab="RSS2/n",cex.lab=1.3,
@@ -186,7 +190,6 @@ to_use <- distance <= 120
 par(mar=c(5,5,1,1))
 x <- distance[cl=="rr" & to_use]
 y <- 10^((features[cl=="rr" & to_use,"mu"])/5 + 1)/1000
-a <- features[cl=="rr" & to_use,"a"]
 lim <- c(0,120)
 plot(x,y,
      xlab="Sesar 2010 Distance (kpc)",
@@ -209,6 +212,50 @@ dev.off()
 
 
 
+####
+## plot distance density for correct and incorrect period estimates
+
+par(mar=c(5,5,1,1))
+x <- distance[cl=="rr"]
+y <- 10^((features[cl=="rr","mu"])/5 + 1)/1000
+
+pc <- abs(dat[cl=="rr","period"] /periods[cl=="rr"] - 1) < .01
+
+
+d1 <- density(y[pc]/x[pc],bw="SJ")
+d2 <- density(y[!pc]/x[!pc],bw="SJ")
+ylim <- range(c(d1$y,d2$y))
+xlim <- c(0.93,1.07)
+pdf("distance_period_kde.pdf",height=5,width=6)
+par(mar=c(5,5,1,1))
+plot(0,0,col=0,
+     xlab="Distance Estimate / Distance True",xlim=xlim,ylim=ylim,
+     ylab="Density",cex.lab=1.3,cex.axis=1.3)
+points(d1$x,d1$y,col='black',lwd=2,type='l')
+points(d2$x,d2$y,col='red',lwd=2,type='l')
+legend("topleft",c(expression(abs(hat(p)/p-1) < .01),expression(abs(hat(p)/p-1) > .01)),
+       col=1:2,lwd=2,cex=1.3)
+dev.off()
+
+sd(y[pc]/x[pc])
+sd(y[!pc]/x[!pc])
+summary(y[pc]/x[pc])
+summary(y[!pc]/x[!pc])
+
+
+
+
+
+dev.new()
+hist(y[!pc]/x[!pc])
+a
+
+plot(log10(features[,"a"]),features[,"period"],col=cl.plot)
+dev.new()
+plot(log10(features[,"a"]),features[,"period"],col=cl.plot)
+points(log10(dat$a[cl=="rr"][!pc]),dat$period[cl=="rr"][!pc],col="blue",pch=19,cex=1.5)
+
+## why do lightcurves with the wrong period have as good (or better distances?)
 
 
 ## rr lyrae misclassified as non-rr lyrae tended to be further away, as expected
