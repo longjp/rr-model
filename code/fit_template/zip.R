f7578d4a8d46d744f72c4e525d7606fb61bf6ba2
James Long <jlong@stat.tamu.edu>
2018-01-04 14:58:53 -0600

    updated zipping of fitting functions

--- code/fit_template/zip.R
+++ code/fit_template/zip.R
@@ -10,13 +10,10 @@ unlink(paste0(direct,".zip"))
 
 ## move files
 dir.create(direct)
-fs <- c("fit_template.R","kappa.py","template.R","template.py",
+fs <- c("fit_template.R","kappa.py","demo_R.ipynb","demo_python.ipynb",
         "template_sdss.RData","template_des.RData","LC_4099.dat",
-        "LC_999886.dat","LC_402316.dat","LC_402316_des.dat","readme_zip")
+        "LC_999886.dat","LC_402316.dat","LC_402316_des.dat","readme")
 file.copy(from=fs,to=direct,recursive=TRUE)
-
-file.rename(from=paste0(direct,"/readme_zip"),
-            to=paste0(direct,"/readme"))
 
 ## zip and remove folder
 zip(paste0(direct,".zip"),direct)
