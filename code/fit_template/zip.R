79c152a5e4d604a02c0a6c1a4d658c41de9b7bb1
James Long <jlong@stat.tamu.edu>
2018-02-22 14:05:19 -0600

    setting up subdirectory with just templates

--- code/fit_template/zip.R
+++ code/fit_template/zip.R
@@ -2,19 +2,19 @@
 ## for fitting the model in R and python
 rm(list=ls())
 
-direct <- "template"
+direct <- "rr-templates"
 
-## remove old zipped info
-unlink(direct,recursive=TRUE)
-unlink(paste0(direct,".zip"))
+dir.create(direct,showWarnings=FALSE)
+fs <- list.files(direct,full.names=TRUE)
+unlink(fs,recursive=TRUE)
 
 ## move files
-dir.create(direct)
-fs <- c("fit_template.R","kappa.py","demo_R.ipynb","demo_python.ipynb",
+fs <- c("fit_template.R","demo_R.ipynb","demo_python.ipynb",
         "template_sdss.RData","template_des.RData","LC_4099.dat",
-        "LC_999886.dat","LC_402316.dat","LC_402316_des.dat","readme")
+        "LC_999886.dat","LC_402316.dat","LC_402316_des.dat","README.md",
+        "figs/")
 file.copy(from=fs,to=direct,recursive=TRUE)
 
 ## zip and remove folder
-zip(paste0(direct,".zip"),direct)
-unlink(direct,recursive=TRUE)
+##zip(paste0(direct,".zip"),direct)
+##unlink(direct,recursive=TRUE)
