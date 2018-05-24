2985d8052429cd5de80b58f97be4504650a229e9
james <jlong@stat.tamu.edu>
2018-05-24 09:52:56 -0500

    updated file copying

--- code/fit_template/copy.R
+++ code/fit_template/copy.R
@@ -1,18 +1,24 @@
-## creates archive with all necessary files
-## for fitting the model in R and python
+## moves files into separate template fitting only repo
+## for uploading to github
 rm(list=ls())
 
-direct <- "rr-templates"
 
-dir.create(direct,showWarnings=FALSE)
-fs <- list.files(direct,full.names=TRUE)
-unlink(fs,recursive=TRUE)
+args = commandArgs(trailingOnly=TRUE)
+
+direct <- args[1]
+print(paste0("copying files to: ",direct))
+
+## dir.create(direct,showWarnings=FALSE)
+## fs <- list.files(direct,full.names=TRUE)
+## unlink(fs,recursive=TRUE)
 
 ## move files
 fs <- c("fit_template.R","demo_R.ipynb","demo_python.ipynb",
         "template_sdss.RData","template_des.RData","LC_4099.dat",
         "LC_999886.dat","LC_402316.dat","LC_402316_des.dat","README.md",
         "figs/","LICENSE")
+print(fs)
+print(direct)
 file.copy(from=fs,to=direct,recursive=TRUE)
 
 ## zip and remove folder
